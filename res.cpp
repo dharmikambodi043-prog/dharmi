0165f48a0c005cb63ea0a219d0d43611a25d6570
dharmikambodi043-prog <dharmikambodi043@gmail.com>
2026-04-13 15:49:08 +0530

    Add files via upload

---
+++ res.cpp
@@ -0,0 +1,67 @@
+#include <iostream.h>
+using namespace std;
+
+struct Student {
+    int id;
+    string name;
+    float marks;
+};
+
+int main() {
+    Student s[100];
+    int n = 0;
+    int choice;
+
+    do {
+        cout << "\n--- Student Management System ---\n";
+        cout << "1. Add Student\n";
+        cout << "2. Show Students\n";
+        cout << "3. Exit\n";
+        cout << "Enter choice: ";
+        cin >> choice;
+
+        switch(choice) {
+
+        case 1:
+            cout << "Enter ID: ";
+            cin >> s[n].id;
+            cout << "Enter Name: ";
+            cin >> s[n].name;
+            cout << "Enter Marks: ";
+            cin >> s[n].marks;
+            n++;
+            cout << "Student Added Successfully!\n";
+            break;
+
+        case 2:
+            for(int i = 0; i < n; i++) {
+                cout << "\nID: " << s[i].id;
+                cout << "\nName: " << s[i].name;
+                cout << "\nMarks: " << s[i].marks;
+
+                // Grade logic
+                if(s[i].marks >= 80)
+                    cout << "\nGrade: A";
+                else if(s[i].marks >= 60)
+                    cout << "\nGrade: B";
+                else if(s[i].marks >= 40)
+                    cout << "\nGrade: C";
+                else
+                    cout << "\nGrade: Fail";
+
+                cout << "\n------------------";
+            }
+            break;
+
+        case 3:
+            cout << "Exiting...";
+            break;
+
+        default:
+            cout << "Invalid choice!";
+        }
+
+    } while(choice != 3);
+
+    return 0;
+}
